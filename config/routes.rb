299eb2a68fc00fe1fcff03236ae2f24e5f5fdb4f
Tyna Huynh <tynahuynh@gmail.com>
2017-02-05 08:15:18 +0000

    Building the about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'quotes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :quotes
+  get 'about', to: 'quotes#about'
 end
